6cdcdaac45252fdefa0f7924abb05dab212da739
EB1811 <44376246+EB1811@users.noreply.github.com>
2020-10-16 13:24:01 +0100

    Created React App

---
+++ ProjectFiles/react_files/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        CE301 Project
+      </p>
+    </div>
+  );
+}
+
+export default App;
