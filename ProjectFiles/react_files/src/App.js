df288632f785ff4877c503433718917c26a99f8a
EB1811 <44376246+EB1811@users.noreply.github.com>
2020-10-24 11:31:58 +0100

    Added 2 more pages to React Router.

--- ProjectFiles/react_files/src/App.js
+++ ProjectFiles/react_files/src/App.js
@@ -8,6 +8,8 @@ import { Switch, Route, useLocation } from 'react-router-dom';
 import MainPage from './Components/MainPage/MainPage';
 import LearnMainPage from './Components/Learn/LearnMainPage';
 import DistributeMainPage from './Components/Distribute/DistributeMainPage';
+import DistributeRentPage from './Components/Distribute/DistributeRentPage';
+import DistributeGoodsPage from './Components/Distribute/DistributeGoodsPage';
 
 function App() {
   // Contains info about route.
@@ -19,6 +21,8 @@ function App() {
           <Route exact path='/' component={MainPage}/>
           <Route exact path='/Learn' component={LearnMainPage}/>
           <Route exact path='/Distribute' component={DistributeMainPage}/>
+          <Route exact path='/Distribute/Rent' component={DistributeRentPage}/>
+          <Route exact path='/Distribute/Goods' component={DistributeGoodsPage}/>
       </Switch>
     </div>
   );
