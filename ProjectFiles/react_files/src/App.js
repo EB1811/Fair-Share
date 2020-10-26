d42689545357616e8b83b78cbc2e47f03dcdeb5d
EB1811 <44376246+EB1811@users.noreply.github.com>
2020-10-26 11:30:43 +0000

    CSS changes

--- ProjectFiles/react_files/src/App.js
+++ ProjectFiles/react_files/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './App.scss';
 
 // React Router
 import { Switch, Route, useLocation } from 'react-router-dom';
--- ProjectFiles/react_files/src/Components/MainPage/MainPage.js
+++ ProjectFiles/react_files/src/Components/MainPage/MainPage.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import '../../App.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 // React Router
 import { Link } from 'react-router-dom';
 
 const MainPage = () => {
     return (
-        <div className="App">
-             <h1>
-                CE301 Project
-            </h1>
-            <Link to='/Learn'>Learn</Link><br/>
-            <Link to='/Distribute'>Distribute</Link>
-        </div>
+        <Container fluid>
+            <Container fluid>
+                <h1 className="Title">
+                    CE301 Project
+                </h1>
+            </Container>
+
+            <Container fluid>
+                <Button variant="primary" size="lg" block>
+                    <Link style={{textDecoration: "none"}} to='/Learn'><h1>Learn</h1></Link>
+                </Button>
+                <Button btn="custom1" variant="primary" size="lg" block>
+                    <Link style={{textDecoration: "none"}} to='/Distribute'><h1>Distribute</h1></Link>
+                </Button>
+            </Container>
+        </Container>
     )
 }
 
