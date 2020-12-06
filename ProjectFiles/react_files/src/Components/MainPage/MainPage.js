fb3271c915a3bdf6f33df66781c52ad5abb6be34
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2020-12-06 14:01:50 +0000

    Added logos to main page

--- ProjectFiles/react_files/src/Components/MainPage/MainPage.js
+++ ProjectFiles/react_files/src/Components/MainPage/MainPage.js
@@ -5,22 +5,39 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 
 // Images
 import bgImg from '../../Images/group-division-rent-happy.jpg';
+import logo1 from '../../Images/fair_ICON2.svg';
 
 // React Router
 import { Link } from 'react-router-dom';
 
 const MainPage = () => {
+    // ADD ATTRIBUTIONS:
+    // <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
     return (
         <div>
         <Container fluid style={{height: "100vh", backgroundImage: `url(${bgImg})`, position: "absolute"}} className="bgImage"/>
         <Container fluid style={{height: "100vh"}}>
-            <Row className="align-items-center h-100">
-                <Col xs="12" style={{marginTop: "5%"}}><h1 className="Title">CE301 Project</h1></Col>
-                <Col xs="12">
+            <Row className="align-items-center h-100 justify-content-sm-center">
+                <Col xs="12" style={{marginTop: "1.5%"}}><h1 className="Title">CE301 Project</h1></Col>
+                <Row className="w-50" style={{marginTop: ""}}>
+                    <Col style={{display: "inline"}}>
+                        <img src={logo1} alt="icon-fair-division" style={{width: "125px", height: "125px"}}/>
+                        <h1 className="iconDesc">Fair</h1>
+                    </Col>
+                    <Col style={{display: "inline"}}>
+                        <img src={logo1} alt="icon-fair-division" style={{width: "125px", height: "125px"}}/>
+                        <h1 className="iconDesc">Personalised</h1>
+                    </Col>
+                    <Col style={{display: "inline"}}>
+                        <img src={logo1} alt="icon-fair-division" style={{width: "125px", height: "125px"}}/>
+                        <h1 className="iconDesc">Easy</h1>
+                    </Col>
+                </Row>
+                <Col xs="12" style={{marginTop: "1%"}}>
                     <Link style={{textDecoration: "none"}} to='/Distribute'>
                         <Button variant="primary" size="lg" style={{borderColor: '#ffffff'}}>
                             <h1 style={{}}>Begin</h1>
