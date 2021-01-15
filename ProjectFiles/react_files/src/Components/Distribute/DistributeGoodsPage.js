0f9ddd8f9d53e1bca8f271a7eadeddfe7fdae411
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2021-01-15 15:47:28 +0000

    Distribute goods css fix (min height).

--- ProjectFiles/react_files/src/Components/Distribute/DistributeGoodsPage.js
+++ ProjectFiles/react_files/src/Components/Distribute/DistributeGoodsPage.js
@@ -10,14 +10,8 @@ import DistributeEntry from './DistributeEntry';
 
 const DistributeGoodsPage = () => {
     return (
-        <Container fluid style={{height: "100vh"}}>
-            <Row className="align-items-center h-100 justify-content-center divBlockWithContentPrimary">
-                <Row className="w-100 justify-content-center" style={{marginBottom: "12.5%", padding: "4% 0 6% 0"}}>
-                    <Col xs={12} lg={10}>
-                        <DistributeEntry goodType={"Goods"}/>
-                    </Col>
-                </Row>
-            </Row>
+        <Container fluid className="divBlockWithContentTertiary min-vh-100">
+            <DistributeEntry goodType={"Goods"}/>
         </Container>
     )
 }
