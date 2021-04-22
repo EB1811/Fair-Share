6d8f11b228e710b1783e209c7c2ea6da931b7efe
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2021-04-22 11:29:36 +0100

    Added cypress-firebase as a dev dependencies

--- ProjectFiles/react_files/cypress/support/commands.js
+++ ProjectFiles/react_files/cypress/support/commands.js
@@ -23,3 +23,24 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Add cypress-firebase commands.
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import { attachCustomCommands } from "cypress-firebase";
+
+const fbConfig = {
+    apiKey: "AIzaSyD06iZQiikFSbQ2cuQ3fKbtBVWvHQz0w7k",
+    authDomain: "fairshare-48f9f.firebaseapp.com",
+    projectId: "fairshare-48f9f",
+    storageBucket: "fairshare-48f9f.appspot.com",
+    messagingSenderId: "1097496332317",
+    appId: "1:1097496332317:web:7c6de0506d954ddf961ef4",
+    measurementId: "G-EQK1GXWT16",
+};
+
+firebase.initializeApp(fbConfig);
+
+attachCustomCommands({ Cypress, cy, firebase });
